be7436e49db114229e28533a7d66369bea6c19b8
soluchin <rafisolichin@gmail.com>
2022-07-22 10:46:53 +0700

    get data using thymeleaf, localhost:1882/thymeleaf

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -1,10 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.SkripsiModel;
+import com.example.demo.repository.ISkripsiRepository;
+import com.example.demo.service.ISkripsiService;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @Controller
 public class ViewController {
+	
+	@Autowired
+	ISkripsiService skripsiservice;
+	
 	@RequestMapping("/view")
 	public String view() {
 		return "/home";
@@ -23,5 +37,15 @@ public class ViewController {
 	@RequestMapping("learnjquery")
 	public String learnjqury() {
 		return "/learnjquery";
+	}
+	
+	@RequestMapping("thymeleaf")
+	public String thymeleaf(Model model) {
+		List<SkripsiModel> skripsiModelList= new ArrayList<SkripsiModel>();
+		skripsiModelList= skripsiservice.readAllData();
+		
+		model.addAttribute("skripsi", skripsiModelList);
+		
+		return "/thymeleaf";
 	}
 }
